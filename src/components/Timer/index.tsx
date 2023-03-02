d50384091651a8b5689257888f4b884809a4179b
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2023-03-02 10:38:08 +0300

    feat: freeze timer on game end

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import { Counter } from '../Counter';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
 type TimerProps = {
   isActive: boolean,
 };
 export const Timer = ({ isActive }: TimerProps) => {
   const [counter, setCounter] = useState(0);
+  const { res } = useSelector((state: RootState) => state.game);
 
   useEffect(() => {
     let interval: NodeJS.Timer;
@@ -14,10 +17,12 @@ export const Timer = ({ isActive }: TimerProps) => {
         setCounter((counter) => counter + 1);
       }, 1000);
     } else {
-      setCounter(0);
+      if (!res) {
+        setCounter(0);
+      }
     }
     return () => clearInterval(interval);
-  }, [isActive, counter]);
+  }, [isActive, counter, res]);
 
   return <Counter n={counter} />;
 };
